06095aed8acdce4e39270001014f7b03f0a0c043
ntlong1801 <ntlongcbl@gmail.com>
2023-11-19 12:37:51 +0700

    2nd

--- src/orn.config.ts
+++ src/orn.config.ts
@@ -1,12 +1,14 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { User } from './user/user.entity';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 export const config: TypeOrmModuleOptions = {
   type: 'postgres',
-  port: 5432,
-  host: 'localhost',
-  username: 'postgres',
-  password: '123',
-  database: 'AWP_BE',
+  port: parseInt(process.env.DB_PORT) || 3000,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   synchronize: true,
   entities: [User],
 };
