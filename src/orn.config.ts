2acb948188726e76624e173917dbc312b6b92adf
ntlong1801 <ntlongcbl@gmail.com>
2023-11-19 14:07:01 +0700

    adads

--- src/orn.config.ts
+++ src/orn.config.ts
@@ -2,13 +2,17 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { User } from './user/user.entity';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
-export const config: TypeOrmModuleOptions = {
-  type: 'postgres',
-  port: parseInt(process.env.DB_PORT) ?? 3000,
+
+const db_options = {
   host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT) ?? 3000,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  synchronize: false,
+  database: process.env.DB_NAME,
+};
+export const config: TypeOrmModuleOptions = {
+  type: 'postgres',
+  ...db_options,
+  synchronize: true,
   entities: [User],
 };
